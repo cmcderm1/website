1f257b19a038814b02c0396d0fb3be634c1b0f7e
cmcderm1 <44779832+cmcderm1@users.noreply.github.com>
2018-12-21 17:46:54 -0500

    Create work.js

---
+++ work.js
@@ -0,0 +1,12 @@
+var works = [
+  "img/deer_network_sample.jpg",
+  "img/wealth_shield.jpg",
+  "img/masrath_sample.jpg",
+  "img/conscious_collab_home.jpg",
+  "img/healing_hearts.jpg",
+  "img/prairie_sample.png",
+  "img/CDR_Give.png",
+  "img/htaba.png"
+];
+
+
