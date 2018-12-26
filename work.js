f3b548846072a62cd8545b9b3f6de36ddc1d67b8
cmcderm1 <44779832+cmcderm1@users.noreply.github.com>
2018-12-26 16:11:08 -0500

    Update work.js

--- work.js
+++ work.js
@@ -1,12 +1,43 @@
 var works = [
-  "img/deer_network_sample.jpg",
-  "img/wealth_shield.jpg",
-  "img/masrath_sample.jpg",
-  "img/conscious_collab_home.jpg",
-  "img/healing_hearts.jpg",
-  "img/prairie_sample.png",
-  "img/CDR_Give.png",
-  "img/htaba.png"
-];
-
+  {
+    title: "First Project",
+    pic: "img/deer_network_sample.jpg",
+    url: "http://deer.network"
+  },
+  {
+    title: "Second Project",
+    pic: "img/wealth_shield.jpg",
+    url: "http://wealthshield.co/"
+  },
+  {
+    title: "Third Project",
+    pic: "img/masrath_sample.jpg",
+    url: "http://mastrath.com"
+  },
+  {
+    title: "Fourth Project",
+    pic: "img/conscious_collab_home.jpg",
+    url: "http://conscious-collaborative.com"
+  },
+  {
+    title: "Fifth Project",
+    pic: "img/healing_hearts.jpg",
+    url: "https://healingyourheartministry.com/"
+  },
+  {
+    title: "Sixth Project",
+    pic: "img/prairie_sample.png",
+    url: "https://prairiesonconsulting.com"
+  },
+  {
+    title: "Seventh Project",
+    pic: "img/CDR_Give.png",
+    url: "https://cdresponse.org/"
+  },
+  {
+    title: "Eighth Project",
+    pic: "img/htaba.png",
+    url: "https://htaba.com"
+  }
+]
 
